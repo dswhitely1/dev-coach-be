60b367e5abb324e797231da21feaf9a65073813a
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-12-11 20:13:06 +0100

    added knexfile

---
+++ knexfile.js
@@ -0,0 +1,50 @@
+require("dotenv").config(); // load .env variables
+
+module.exports = {
+  development: {
+    client: "pg",
+    connection: process.env.DATABASE_DEV,
+    useNullAsDefault: true, // used to avoid warning on console
+    migrations: {
+      directory: "./data/migrations",
+      tableName: "dbmigrations"
+    },
+    seeds: { directory: "./data/seeds" }
+  },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations",
+      tableName: "dbmigrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  },
+  staging: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations",
+      tableName: "dbmigrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  },
+  testing: {
+    client: "pg",
+    connection: process.env.DATABASE_TEST,
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations",
+      tableName: "dbmigrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  }
+};
