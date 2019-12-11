3cde040ce2f0f96e037edb35b40a9ae1c238c711
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-12-11 19:42:34 +0100

    add index.js file

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+
+server.get('/', (req, res) => {
+  res.status(200).json({ message: 'Quality Hub API' });
+});
+
+server.all('*', (req, res) => {
+  res.status(404).send({
+    error: 'The resource you are looking for does not exist',
+  });
+});
+
+const port = process.env.PORT || 5000;
+// server.listen(port, console.log(`Listening on Port ${port}`));
+
+/* 
+  With respect to listen EADDRINUSE :::5000 error returned when testing
+  The problem is this: once the execution of the first test ends, the server is still listening on the port 5000. So when we require('../index') again in the second test file, it errors out because is port is still in use by the previous test file’s process.
+  One simple fix for this issue is wrapping our app.listen() in a condition to check if the environment is a test environment or not. In a test environment, when running the server through Supertest, we do not really need to have the app listen on a network port.
+  For more read https://blog.campvanilla.com/jest-expressjs-and-the-eaddrinuse-error-bac39356c33a
+*/
+if (process.env.NODE_ENV !== 'test') {
+  server.listen(port, console.log(`Listening on Port ${port}`));
+}
+module.exports = server;
