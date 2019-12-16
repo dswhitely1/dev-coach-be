8b4a8fa78853a4aac9df43bcb01906bdf8fbb225
Liam Sutton <devliam90@gmail.com>
2019-12-16 16:30:35 +0000

    configure db migrations and seeds

--- data/migrations/20191216140627_users-table.js
+++ data/migrations/20191216140627_users-table.js
@@ -1,4 +1,3 @@
-
 exports.up = function(knex) {
   return knex.schema.createTable('users', table => {
     table.increments();
@@ -7,10 +6,15 @@ exports.up = function(knex) {
     table.string('email', 128).notNullable();
     table.string('password', 50).notNullable();
     table.string('location');
-    table.integer('user_role_id').notNullable().unsigned().references('id').inTable('user_roles');
+    table
+      .integer('user_role_id')
+      .notNullable()
+      .unsigned()
+      .references('id')
+      .inTable('user_roles');
   });
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTableIfExists('users')
+  return knex.schema.dropTableIfExists('users');
 };
