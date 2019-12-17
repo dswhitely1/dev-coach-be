c03b42e1555165678b81c0b0458291e4abf8d7e3
Funmi7 <talabifunmilayo@gmail.com>
2019-12-17 23:55:19 +0100

    Added tests for register endpoint

--- _test_/user-controllers.spec.js
+++ _test_/user-controllers.spec.js
@@ -1,29 +1,47 @@
 const request = require('supertest');
 // const bcrypt = require('bcryptjs')
-const db = require('../data/dbConfig');
+// const db = require('../data/dbConfig');
 const server = require('../index');
 
+// beforeEach(async () => {
+//   await db('users_table').truncate()
+// })
 
-beforeEach(async () => {
-  await db('users_table').truncate()
-})
+const testUser = {
+  first_name: 'fun',
+  last_name: 'tee',
+  email: 'funtee@gmail.com',
+  password: '12345',
+};
 
 describe('usersController', () => {
   describe('POST /user/register', () => {
     test('Correctly creates new user', async () => {
-      const response = await request(server).post('/user/register')
-      .send({
+      const response = await request(server)
+        .post('/user/register')
+        .send(testUser);
+      expect(response.status).toBe(201);
+      expect(response.body).toHaveProperty('message');
+      expect(response.body).toHaveProperty('token');
+      expect(response.body).toHaveProperty('user_id');
+    });
+
+    test('Throws an error for missing data', async () => {
+      const invalidUser = {
         first_name: 'fun',
         last_name: 'tee',
-        email: 'funtee@gmail.com',
+        email: '',
         password: '12345',
-        user_role_id: 1
+      };
+
+      const response = await request(server)
+      .post('/user/register')
+      .send(invalidUser);
+      expect(response.status).toBe(400);
+      expect(response.body).toBeInstanceOf(Object);
+      expect(response.body).toEqual({
+      message: 'Please make sure required fields are filled in.'
       })
-   
-      expect(response.status).toBe(201);
-    })
-    
-  })
-})
-
-
+    });
+  });
+});
