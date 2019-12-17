a9993e0512cebee6ea3fac897e5199f2a96c1f59
Liam Sutton <devliam90@gmail.com>
2019-12-17 12:54:44 +0000

    update check-auth middleware

--- src/resources/routers/user-router.js
+++ src/resources/routers/user-router.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 
 const userController = require('../controllers/user-controllers');
+const checkAuth = require('../../middlewares/check-auth');
 
 router.post('/register', userController.register);
 router.post('/login', userController.login);
-router.delete('/:id', userController.delete);
+router.delete('/:id', checkAuth, userController.delete);
 
 module.exports = router;
