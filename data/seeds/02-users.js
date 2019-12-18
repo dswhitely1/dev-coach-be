e80a6c810f239bb35c1400de52a1b663bd5f160c
Liam Sutton <devliam90@gmail.com>
2019-12-18 14:13:58 +0000

    update seed files

--- data/seeds/02-users.js
+++ data/seeds/02-users.js
@@ -1,44 +1,44 @@
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('users_table')
+  return knex('users')
     .del()
     .then(function() {
       // Inserts seed entries
-      return knex('users_table').insert([
+      return knex('users').insert([
         {
           first_name: 'Jayne',
           last_name: 'Carmichael Norrie',
           email: 'jayne@musicisourforte.co.uk',
           password: 'chico',
-          user_role_id: 2,
+          role_id: 2,
         },
         {
           first_name: 'Liam',
           last_name: 'Sutton',
           email: 'liam@google.com',
           password: 'liam',
-          user_role_id: 2,
+          role_id: 2,
         },
         {
           first_name: 'Funmi',
           last_name: 'Talabi',
           email: 'funmi@google.com',
           password: 'funmi',
-          user_role_id: 2,
+          role_id: 2,
         },
         {
           first_name: 'Bob',
           last_name: 'Smith',
           email: 'bob@google.com',
           password: 'bob',
-          user_role_id: 1,
+          role_id: 1,
         },
         {
           first_name: 'Lizzo',
           last_name: 'Smith',
           email: 'lizzo@google.com',
           password: 'lizzo',
-          user_role_id: 1,
+          role_id: 1,
         },
       ]);
     });
