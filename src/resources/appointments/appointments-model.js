206e7664a603508220d7dde33b71c82340345c9d
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-12-18 22:38:33 +0100

    code indentation

--- src/resources/appointments/appointments-model.js
+++ src/resources/appointments/appointments-model.js
@@ -5,18 +5,28 @@ async function get_appointments(role, id) {
   let appointments;
   if (role === 1) {
     appointments = await db('users')
-    .join('students', 'students.user_id', '=', 'users.id')
-    .join('appointments', 'appointments.student_id', '=', 'students.id')
-    .where('users.id', '=', id)
+      .join('students', 'students.user_id', '=', 'users.id')
+      .join(
+        'appointments',
+        'appointments.student_id',
+        '=',
+        'students.id',
+      )
+      .where('users.id', '=', id);
   } else {
     appointments = await db('users')
-    .join('coaches', 'coaches.user_id', '=', 'users.id')
-    .join('appointments', 'appointments.coach_id', '=', 'coaches.id')
-      .where('users.id', '=', id)
+      .join('coaches', 'coaches.user_id', '=', 'users.id')
+      .join(
+        'appointments',
+        'appointments.coach_id',
+        '=',
+        'coaches.id',
+      )
+      .where('users.id', '=', id);
   }
   return appointments;
 }
 
 module.exports = {
-  get_appointments
+  get_appointments,
 };
