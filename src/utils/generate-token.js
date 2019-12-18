2bec094a4a0184c9ff43e53fce6a7b005c45b00b
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-12-18 13:48:32 +0100

    resolve merge conflict

--- src/utils/generate-token.js
+++ src/utils/generate-token.js
@@ -9,15 +9,11 @@ module.exports = user => {
     expiresIn: '1d',
   };
 
-<<<<<<< HEAD
-  const result = jwt.sign(payload, process.env.SECRET || "Hello", options);
-=======
   const result = jwt.sign(
     payload,
     process.env.SECRET || 'Hello',
     options,
   );
->>>>>>> 6cca2ade58659c4181d53d0abd9df83b7f44b5fe
 
   return result;
 };
