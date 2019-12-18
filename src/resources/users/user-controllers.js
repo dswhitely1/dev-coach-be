bbf840197893a62926301ec826dbba4c9848d978
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-12-18 21:50:51 +0100

    add controller for get appointments

--- src/resources/users/user-controllers.js
+++ src/resources/users/user-controllers.js
@@ -95,3 +95,16 @@ exports.coaches = async (req, res) => {
     res.status(500).json({ message: 'Could not find any coaches' });
   }
 };
+
+exports.appointments = async (req, res) => {
+  try {
+    const appointments = await Users.get_appointments(req.body.role, req.params.id);
+    if (appointments) {
+      res.status(200).json({
+        appointments,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Could not find user' });
+  }
+};
