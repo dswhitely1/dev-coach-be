225ccb140886a10a49c7f45f1881eb613f92853b
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-12-18 13:44:49 +0100

    code indentation for index test

--- _test_/index.spec.js
+++ _test_/index.spec.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const server = require('../index');
 
 describe('index', () => {
-  describe('[GET] / endpoint', () => {})
+  describe('[GET] / endpoint', () => {});
   test('the db env is testing', () => {
     expect(process.env.DB_ENV).toBe('testing');
   });
@@ -17,19 +17,19 @@ describe('index', () => {
       .get('/')
       .expect(200)
       .expect('Content-Type', /json/);
-  })
+  });
 
   test(' NO / expect 200 ', () => {
     return request(server)
       .get('')
-      .expect(200)
-  })
+      .expect(200);
+  });
 
   test(' / test expect 404 ', () => {
     return request(server)
       .get('/test')
-      .expect(404)
-  })
+      .expect(404);
+  });
 
   test('welcome message displayed ', () => {
     return request(server)
@@ -39,11 +39,6 @@ describe('index', () => {
         expect(res.status).toEqual(200);
         expect(res.body.message).toBe('Welcome to the Dev Coach API');
         expect(res.body).toHaveProperty('message');
-  })
-
-
-
-})
-
-  
+      });
+  });
 });
