23018b2c773c8c333f558235701a44a273d054f1
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-12-18 22:14:40 +0100

    bugfix in appointments model

--- src/resources/appointments/appointments-model.js
+++ src/resources/appointments/appointments-model.js
@@ -5,13 +5,13 @@ async function get_appointments(role, id) {
   let appointments;
   if (role === 1) {
     appointments = await db('users')
-      .join('students', 'students.user_id', '=', 'users.id')
-      .join('appointments', 'appointments.user_id', '=', 'users.id')
-      .where('users.id', '=', id)
+    .join('students', 'students.user_id', '=', 'users.id')
+    .join('appointments', 'appointments.student_id', '=', 'students.id')
+    .where('users.id', '=', id)
   } else {
     appointments = await db('users')
-      .join('coaches', 'coaches.user_id', '=', 'users.id')
-      .join('appointments', 'appointments.user_id', '=', 'users.id')
+    .join('coaches', 'coaches.user_id', '=', 'users.id')
+    .join('appointments', 'appointments.coach_id', '=', 'coaches.id')
       .where('users.id', '=', id)
   }
   return appointments;
