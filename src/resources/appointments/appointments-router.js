ed6031edd61ba8dba188f52760f89e175cf3f3d2
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-12-18 22:45:20 +0100

    added checkAuth middleware

--- src/resources/appointments/appointments-router.js
+++ src/resources/appointments/appointments-router.js
@@ -3,6 +3,6 @@ const router = require('express').Router();
 const appointmentsController = require('./appointments-controllers');
 const checkAuth = require('../../utils/check-auth');
 
-router.get('/:id', appointmentsController.appointments); // add checkauth
+router.get('/:id', checkAuth, appointmentsController.appointments);
 
 module.exports = router;
