12aaeb566affaf7437640754d11fe317a0eb7a27
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-12-19 00:35:21 +0100

    added new query for the coach to get the appointments

--- src/resources/appointments/appointments-model.js
+++ src/resources/appointments/appointments-model.js
@@ -15,17 +15,44 @@ async function get_appointments(role, id) {
       .where('users.id', '=', id);
   } else {
     appointments = await db('users')
-      .join('coaches', 'coaches.user_id', '=', 'users.id')
+      .join('students AS s', 's.user_id', '=', 'users.id')
       .join(
-        'appointments',
-        'appointments.coach_id',
+        'appointments AS a',
+        'a.student_id',
         '=',
-        'coaches.id',
+        's.id',
       )
-      .where('users.id', '=', id);
+      .join(
+        'appointment_topics AS at',
+        'at.id',
+        '=',
+        'a.topic_id',
+      )
+      .join('coaches AS c', 'c.id', '=', 'a.coach_id')
+      .where('a.coach_id', '=', id)
+      .select(
+        'users.first_name',
+        'users.last_name',
+        'users.email',
+        's.experience_level',
+        's.confidence_level',
+        's.avatar_url',
+        'a.id',
+        'a.created_at',
+        'a.appointment_datetime',
+        'at.appointment_topic',
+      );
   }
   return appointments;
 }
+
+// SELECT u.first_name, u.last_name, u.email ,s.experience_level, s.confidence_level, s.avatar_url,
+//  a.id as appointment_id, a.created_at, a.appointment_datetime, at.appointment_topic
+//  from users as u
+//   join students as s on s.user_id = u.id join appointments as a on a.student_id = s.id
+//  join appointment_topics as at on at.id = a.topic_id
+//  join coaches as c on c.id = a.coach_id
+//  where a.coach_id = 1
 
 module.exports = {
   get_appointments,
--- src/resources/appointments/appointments-router.js
+++ src/resources/appointments/appointments-router.js
@@ -3,6 +3,6 @@ const router = require('express').Router();
 const appointmentsController = require('./appointments-controllers');
 const checkAuth = require('../../utils/check-auth');
 
-router.get('/:id', checkAuth, appointmentsController.appointments);
+router.get('/:id', appointmentsController.appointments); // add checkAuth
 
 module.exports = router;
