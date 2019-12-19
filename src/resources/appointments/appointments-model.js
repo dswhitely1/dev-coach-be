e87ff76381567dca75a716b09f54c95c55416734
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-12-19 01:16:05 +0100

    code indentation

--- src/resources/appointments/appointments-model.js
+++ src/resources/appointments/appointments-model.js
@@ -5,47 +5,27 @@ async function get_appointments(role, id) {
   let appointments;
   if (role === 1) {
     appointments = await db('users')
-    .join('coaches AS c', 'c.user_id', '=', 'users.id')
-    .join(
-      'appointments AS a',
-      'a.coach_id',
-      '=',
-      'c.id',
-    )
-    .join(
-      'appointment_topics AS at',
-      'at.id',
-      '=',
-      'a.topic_id',
-    )
-    .where('a.student_id', '=', id)
-    .select(
-      'users.first_name',
-      'users.last_name',
-      'users.email',
-      'c.experience_level',
-      'c.skill_level',
-      'c.avatar_url',
-      'a.id',
-      'a.created_at',
-      'a.appointment_datetime',
-      'at.appointment_topic',
-    );
+      .join('coaches AS c', 'c.user_id', '=', 'users.id')
+      .join('appointments AS a', 'a.coach_id', '=', 'c.id')
+      .join('appointment_topics AS at', 'at.id', '=', 'a.topic_id')
+      .where('a.student_id', '=', id)
+      .select(
+        'users.first_name',
+        'users.last_name',
+        'users.email',
+        'c.experience_level',
+        'c.skill_level',
+        'c.avatar_url',
+        'a.id',
+        'a.created_at',
+        'a.appointment_datetime',
+        'at.appointment_topic',
+      );
   } else {
     appointments = await db('users')
       .join('students AS s', 's.user_id', '=', 'users.id')
-      .join(
-        'appointments AS a',
-        'a.student_id',
-        '=',
-        's.id',
-      )
-      .join(
-        'appointment_topics AS at',
-        'at.id',
-        '=',
-        'a.topic_id',
-      )
+      .join('appointments AS a', 'a.student_id', '=', 's.id')
+      .join('appointment_topics AS at', 'at.id', '=', 'a.topic_id')
       .where('a.coach_id', '=', id)
       .select(
         'users.first_name',
