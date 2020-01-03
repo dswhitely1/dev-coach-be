ec8f08e9142733f9881dd5e2d3617a87f4b1f1cb
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2020-01-03 20:12:33 +0100

    text correction for seed file

--- data/seeds/05-appointment_topics.js
+++ data/seeds/05-appointment_topics.js
@@ -7,7 +7,7 @@ exports.seed = function(knex) {
       return knex('appointment_topics').insert([
         { appointment_topic: 'Frontend' },
         { appointment_topic: 'Backend' },
-        { appointment_topic: 'Algortihms / Data Structes' },
+        { appointment_topic: 'Algorithms / Data Structes' },
         { appointment_topic: 'Behavorial Interview' },
         { appointment_topic: 'System Design' },
         { appointment_topic: 'React' },
