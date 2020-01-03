d5c111a33b5602ff1b1a401482e4400f06c0dc70
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2020-01-03 20:03:37 +0100

    add stripe controller

--- src/resources/payments/payment-controllers.js
+++ src/resources/payments/payment-controllers.js
@@ -1,9 +1,10 @@
-const server = require('express').Router();
-const stripe = require("stripe")("sk_test_j5vEAa5mDDXxAZ0D0KJ3lH3x00tu6gpneX");
-const uuid = require("uuid/v4");
+const stripe = require('stripe')(
+  'sk_test_j5vEAa5mDDXxAZ0D0KJ3lH3x00tu6gpneX',
+);
+const uuid = require('uuid/v4');
 
-server.post("/payment", async (req, res) => {
-  console.log("Request:", req.body);
+exports.stripe = async (req, res) => {
+  console.log('Request:', req.body);
 
   let error;
   let status;
@@ -12,14 +13,14 @@ server.post("/payment", async (req, res) => {
 
     const customer = await stripe.customers.create({
       email: token.email,
-      source: token.id
+      source: token.id,
     });
 
     const idempotency_key = uuid();
     const charge = await stripe.charges.create(
       {
         amount: product.price * 100,
-        currency: "usd",
+        currency: 'usd',
         customer: customer.id,
         receipt_email: token.email,
         description: `Purchased the ${product.name}`,
@@ -30,22 +31,20 @@ server.post("/payment", async (req, res) => {
             line2: token.card.address_line2,
             city: token.card.address_city,
             country: token.card.address_country,
-            postal_code: token.card.address_zip
-          }
-        }
+            postal_code: token.card.address_zip,
+          },
+        },
       },
       {
-        idempotency_key
-      }
+        idempotency_key,
+      },
     );
-    console.log("Charge:", { charge });
-    status = "success";
+    console.log('Charge:', { charge });
+    status = 'success';
   } catch (error) {
-    console.error("Error:", error);
-    status = "failure";
+    console.error('Error:', error);
+    status = 'failure';
   }
 
   res.json({ error, status });
-});
-
-module.exports = server;
+};
