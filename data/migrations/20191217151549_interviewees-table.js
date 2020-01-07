cc890b198db0cb234718db26562696cda17d1887
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2020-01-07 12:58:16 +0100

    add restrictions to string cols

--- data/migrations/20191217151549_interviewees-table.js
+++ data/migrations/20191217151549_interviewees-table.js
@@ -7,7 +7,7 @@ exports.up = function(knex) {
       .unsigned()
       .references('id')
       .inTable('users');
-    table.string('avatar_url');
+    table.string('avatar_url', 50);
     table.integer('experience_level').notNullable();
     table.integer('confidence_level').notNullable();
   });
--- data/migrations/20200107121442_appointments.js
+++ data/migrations/20200107121442_appointments.js
@@ -2,7 +2,7 @@ exports.up = function(knex) {
   return knex.schema.createTable('appointments', table => {
     table.increments();
     table.timestamp('created_at').defaultTo(knex.fn.now());
-    table.string('appointment_datetime');
+    table.string('appointment_datetime', 50);
     table.boolean('canceled').defaultTo(false);
     table
       .integer('coach_id')
