938a8e3dba41b19588ecce784dcd7109e7d0f2b8
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2020-01-07 20:52:03 +0100

    delete comments

--- src/resources/profiles/profile-models.js
+++ src/resources/profiles/profile-models.js
@@ -1,5 +1,4 @@
 const db = require('../../../data/dbConfig');
-const utils = require('../../utils/modelHelpers');
 
 async function get_coaches() {
   const coaches = await db('users').join(
@@ -12,6 +11,5 @@ async function get_coaches() {
 }
 
 module.exports = {
-  // user_details,
   get_coaches,
 };
