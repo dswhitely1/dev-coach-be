79ec734e2a5a0c0d2ac71cb39736938369970c52
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2020-01-07 12:39:36 +0100

    added feedback router

---
+++ src/resources/feedback/feedback-router.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+const feedbackController = require('./feedback-controllers');
+
+router.post('/:id', feedbackController.feedback);
+// id is the student id (we need to consider that we could add a feedback for the coach also in the future)
+module.exports = router;
