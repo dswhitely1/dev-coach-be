895a5c8745f39628aa4fcf3f86ca900a1674c08e
Dom <domeccleston@gmail.com>
2020-01-13 10:35:01 +0000

    correct getCoaches

--- src/resources/profiles/profile-controllers.js
+++ src/resources/profiles/profile-controllers.js
@@ -16,7 +16,7 @@ exports.students = async (req, res) => {
 
 exports.coaches = async (req, res) => {
   try {
-    const coaches = await Profile.getCoaches();
+    const coaches = await Profile.get_coaches();
     if (coaches) {
       res.status(200).json({
         coaches,
