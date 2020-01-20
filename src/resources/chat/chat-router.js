47131ba8811fdea1d12150080b1a3c0366e7ad09
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2020-01-20 21:47:55 +0100

    add createRoom endpoint

--- src/resources/chat/chat-router.js
+++ src/resources/chat/chat-router.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 
 const chatController = require('./chat-controllers');
-const checkAuth = require('../../utils/check-auth');
 
 router.post('/', chatController.createUser);
 router.post('/auth', chatController.auth);
+router.post('/room', chatController.createRoom);
 module.exports = router;
