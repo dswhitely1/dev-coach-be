0e0561b320b3a270dcca21d815960b374a2fffa5
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2020-01-20 13:18:47 +0100

    create chat controller and router

---
+++ src/resources/chat/chat-controllers.js
@@ -0,0 +1,7 @@
+const Chatkit = require('@pusher/chatkit-server');
+
+const chatkit = new Chatkit.default({
+  instanceLocator: 'v1:us1:02d03086-c977-4990-bbb8-d915c9090f74',
+  key:
+    '99c25bda-a15a-4247-8a6c-5c152975240e:IIvhll4k19DorTpn6MYuo3YlNI3qGQjKQuX5F9WX7N4=',
+});
