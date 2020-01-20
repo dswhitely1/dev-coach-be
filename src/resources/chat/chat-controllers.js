ce1eb172f7c41c3b7388e98accdd33a06dd095a7
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2020-01-20 14:03:30 +0100

    add .env variables

--- src/resources/chat/chat-controllers.js
+++ src/resources/chat/chat-controllers.js
@@ -1,30 +1,27 @@
 const Chatkit = require('@pusher/chatkit-server');
 
 const chatkit = new Chatkit.default({
-  instanceLocator: 'v1:us1:02d03086-c977-4990-bbb8-d915c9090f74',
-  key:
-    '99c25bda-a15a-4247-8a6c-5c152975240e:IIvhll4k19DorTpn6MYuo3YlNI3qGQjKQuX5F9WX7N4=',
+  instanceLocator: process.env.PUSHER_INSTANCE,
+  key: process.env.PUSHER_CHAT_KEY,
 });
 
 exports.chat = async (req, res) => {
   const { email } = req.body;
 
-  try {
-    const user = await chatkit.createUser({
+  chatkit
+    .createUser({
       id: email,
       name: email,
+    })
+    .then(() => res.status(201))
+    .catch(error => {
+      if (
+        error.error_type ===
+        'services/chatkit/user/user_already_exists'
+      ) {
+        res.sendStatus(200);
+      } else {
+        res.status(error.statusCode).json(error);
+      }
     });
-
-    if (user) {
-      res.status(201).json({ user });
-    }
-  } catch (error) {
-    if (
-      error.error_type === 'services/chatkit/user/user_already_exists'
-    ) {
-      res.sendStatus(200);
-    } else {
-      res.status(error.statusCode).json(error);
-    }
-  }
 };
--- src/resources/chat/chat-router.js
+++ src/resources/chat/chat-router.js
@@ -3,5 +3,5 @@ const router = require('express').Router();
 const chatController = require('./chat-controllers');
 const checkAuth = require('../../utils/check-auth');
 
-router.post('/:id', checkAuth, chatController.chat);
+router.post('/', chatController.chat);
 module.exports = router;
