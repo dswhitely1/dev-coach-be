a1062f343ec1ea4f8ef743092ea3bf359e26f990
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2020-01-20 13:32:05 +0100

    create chat controller

--- src/resources/chat/chat-controllers.js
+++ src/resources/chat/chat-controllers.js
@@ -5,3 +5,26 @@ const chatkit = new Chatkit.default({
   key:
     '99c25bda-a15a-4247-8a6c-5c152975240e:IIvhll4k19DorTpn6MYuo3YlNI3qGQjKQuX5F9WX7N4=',
 });
+
+exports.chat = async (req, res) => {
+  const { email } = req.body;
+
+  try {
+    const user = await chatkit.createUser({
+      id: email,
+      name: email,
+    });
+
+    if (user) {
+      res.status(201).json({ user });
+    }
+  } catch (error) {
+    if (
+      error.error_type === 'services/chatkit/user/user_already_exists'
+    ) {
+      res.sendStatus(200);
+    } else {
+      res.status(error.statusCode).json(error);
+    }
+  }
+};
