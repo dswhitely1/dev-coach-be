ee409c775543ac60180213e643421965e92b8ef2
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2020-01-25 00:53:44 +0100

    add login controller

--- src/resources/video/video-controllers.js
+++ src/resources/video/video-controllers.js
@@ -12,7 +12,30 @@ const pusher = new Pusher({
 exports.auth = (req, res) => {
   const socketId = req.body.socket_id;
   const channel = req.body.channel_name;
-  console.log('logging in...');
+
   const auth = pusher.authenticate(socketId, channel);
   return res.send(auth);
+};
+
+exports.login = async (req, res) => {
+  const { channel, username } = req.body;
+
+  const channel_index = channels.findIndex(c => c.name === channel);
+  if (channel_index === -1) {
+
+    channels.push({
+      name: channel,
+      users: [username],
+    });
+
+    return res.send('ok');
+  }
+
+  if (channels[channel_index].users.indexOf(username) === -1) {
+    channels[channel_index].users.push(username);
+
+    return res.send('ok');
+  }
+
+  return res.status(500).send('invalid user');
 };
