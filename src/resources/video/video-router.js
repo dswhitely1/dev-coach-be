58531504c01b427818386449feee829502371ec7
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2020-01-25 01:04:00 +0100

    fix routes

--- src/resources/video/video-router.js
+++ src/resources/video/video-router.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const videoController = require('./video-controllers');
 
 router.post('/auth', videoController.auth);
-router.post('/login', videoController.auth);
-router.post('/users', videoController.auth);
+router.post('/login', videoController.login);
+router.post('/users', videoController.users);
 
 module.exports = router;
