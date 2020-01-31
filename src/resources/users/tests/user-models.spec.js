093de413e7987378a95227d881551e1e038b4d17
Liam Sutton <devliam90@gmail.com>
2020-01-31 15:14:51 +0000

    wip

--- src/resources/users/tests/user-models.spec.js
+++ src/resources/users/tests/user-models.spec.js
@@ -7,29 +7,4 @@ describe('Users model', () => {
       expect(users.length).toBe(17);
     });
   });
-
-  describe('.insert()', () => {
-    test('should insert a user successfully', async () => {
-      const user = await Users.add({
-        first_name: 'fun',
-        last_name: 'tee',
-        email: 'funtee@gmail.com',
-        password: '12345',
-      });
-      const users = await Users.find();
-      expect(user).toEqual({
-        avatar_url: '',
-        email: 'funtee@gmail.com',
-        first_name: 'fun',
-        id: 18,
-        last_name: 'tee',
-        location: null,
-        password: '12345',
-        role_id: null,
-        github: null,
-        linkedin: null,
-      });
-      expect(users.length).toBe(18);
-    });
-  });
 });
