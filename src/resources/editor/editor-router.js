21ebaacc9e915c76472c812a38caa922c2cabb97
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2020-02-01 03:13:32 +0100

    bugfix for editor router

--- src/resources/editor/editor-router.js
+++ src/resources/editor/editor-router.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 
 const editorController = require('./editor-controllers');
 
-router.post('/upate', editorController.updateEditor);
+router.post('/update', editorController.updateEditor);
 
 module.exports = router;
