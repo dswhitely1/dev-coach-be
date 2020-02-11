588a043501cb27ec265a6294a2ac1c1fdc282c89
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2020-02-11 13:09:09 +0100

    bugfix

--- src/resources/profiles/profile-router.js
+++ src/resources/profiles/profile-router.js
@@ -4,13 +4,13 @@ const userController = require('./profile-controllers');
 const checkAuth = require('../../utils/check-auth');
 
 router.get('/coaches', checkAuth, userController.coaches);
-router.get('/students', checkAuth, userController.students);
-router.post('/coaches', checkAuth, userController.addCoach);
-router.post('/students', checkAuth, userController.addStudent);
-router.put(
-  '/coachesSettings/:id',
-  checkAuth,
-  userController.updateCoach,
-);
+// router.get('/students', checkAuth, userController.students);
+// router.post('/coaches', checkAuth, userController.addCoach);
+// router.post('/students', checkAuth, userController.addStudent);
+// router.put(
+//   '/coachesSettings/:id',
+//   checkAuth,
+//   userController.updateCoach,
+// );
 
 module.exports = router;
