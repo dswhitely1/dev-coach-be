8e4e96449a6b50fbd6f79c58feb63dc235083c2a
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2020-02-12 01:02:31 +0100

    add checkAuth middleware

--- src/resources/appointments/appointments-router.js
+++ src/resources/appointments/appointments-router.js
@@ -3,9 +3,9 @@ const router = require('express').Router();
 const appointmentsController = require('./appointments-controllers');
 const checkAuth = require('../../utils/check-auth');
 
-router.get('/:id', appointmentsController.appointments);
+router.get('/:id', checkAuth, appointmentsController.appointments);
 router.post('/', checkAuth, appointmentsController.addAppointment);
-router.put('/:id', appointmentsController.updateAppointment);
+router.put('/:id', checkAuth, appointmentsController.updateAppointment);
 router.post(
   '/email',
   checkAuth,
--- src/resources/feedback/feedback-router.js
+++ src/resources/feedback/feedback-router.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const feedbackController = require('./feedback-controllers');
 const checkAuth = require('../../utils/check-auth');
 
-router.get('/:id', feedbackController.feedback);
+router.get('/:id', checkAuth, feedbackController.feedback);
 router.post('/', checkAuth, feedbackController.addFeedback);
 
 module.exports = router;
