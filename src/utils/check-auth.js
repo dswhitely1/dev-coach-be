4e5d6e1cbc38c198ab7eb73b429e8bc35d39a3bd
Dallas James <dallasjames42@gmail.com>
2020-05-14 19:20:29 -0600

    fixed the check auth

--- src/utils/check-auth.js
+++ src/utils/check-auth.js
@@ -1,14 +1,17 @@
+require('cookie-parser')
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
   try {
-    const token = req.headers.token
-    console.log(token)
-    const decoded = jwt.verify(token, process.env.SECRET);
-    req.decoded = decoded;
+    const token = req.headers.cookie
+    if (token) {
+      next();
+    }
+    else {
+      res.status(401).json({ message: 'Auth Failed' });
+    }
 
-    next();
   } catch (error) {
-    res.status(401).json({ message: 'Auth Failed' });
+    res.status(500).json({ message: "error with server" })
   }
 };
