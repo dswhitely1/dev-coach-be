0b064f63f98a18f9e18f300d3ba9eda7cd86a558
Reinoso <jreinoso@amazon.com>
2020-06-03 20:03:20 -0400

    auth debugging

--- src/utils/check-auth.js
+++ src/utils/check-auth.js
@@ -1,22 +1,14 @@
+
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
   try {
-    const {token} = req.cookie;
-       if (!token) {
-      res.status(401).json({message: "User Not logged in"})
-    }
-    jwt.verify(token, process.env.SECRET, (err, decoded) => {
-      if (err) {
-        res.status(401).json({ you: err.message });
-      }  else {
-        req.decoded = decoded;
-        next();
-      }
-      
-    })
+    const token = req.cookies.token
 
-       
+    const decoded = jwt.verify(token, process.env.SECRET);
+    req.decoded = decoded;
+
+    next();
   } catch (error) {
     res.status(401).json({ message: 'Auth Failed' });
   }
