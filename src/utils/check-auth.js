b1bc42f39bfac87a8024006bbb707bee5514f773
Dallas James <dallasjames42@gmail.com>
2020-06-17 18:57:54 -0600

    auth

--- src/utils/check-auth.js
+++ src/utils/check-auth.js
@@ -4,27 +4,23 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
   console.log("Right way",req.cookies) 
-   console.log("take a look",req.headers.cookie)
-   console.log(req)
+  console.log("take a look",req.headers.cookie)
   try {
 
-    const { token } = req.cookies;
-    const token2 = req.headers.authorization
+    const token = req.cookies.token || req.headers.authorization;
 
-       if (!token && !token2) {
-      return res.status(401).json({message: `User Not logged in, ${token && token2}`})
-      
-    }
-    jwt.verify(token || token2, process.env.SECRET, (err, decoded) => {
+    if (!token) {     
+       res.status(401).json({message: `fail ${token}`})
+    } else {
+      jwt.verify(token, process.env.SECRET, (err, decoded) => {
       if (err) {
-        res.status(401).json({ you: `${err.message}, ${token && token2}` });
+        res.status(401).json({ you: `${err.message}` });
         console.log(err)
-      }  else {
         console.log(decoded)
-       
       }
     })
     next();
+    }
   } catch (error) {
     res.status(401).json({ message: 'Auth Failed' });
     console.log(error)
