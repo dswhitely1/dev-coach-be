6572bce668bba476386946fb63105e412d6d325a
Dallas James <dallasjames42@gmail.com>
2020-06-19 19:09:20 -0600

    fixing maintainability

--- src/resources/users/user-model.js
+++ src/resources/users/user-model.js
@@ -53,31 +53,6 @@ async function findByForLogin(EmailOrUsername) {
   return user;
 }
 
-async function findByForUsername(username) {
-  let user = await db('users')
-    .where(username)
-    .first();
-
-  if (user.role_id) {
-    const { id } = user;
-
-    if (user.role_id === 1) {
-      user = await db('users')
-        .join('students', 'students.user_id', '=', 'users.id')
-        .where('users.id', '=', id)
-        .first();
-    } else {
-      user = await db('users')
-        .join('coaches', 'coaches.user_id', '=', 'users.id')
-        .where('users.id', '=', id)
-        .first();
-    }
-    return user;
-  }
-
-  return user;
-}
-
 async function findById(id) {
   const user = await db('users')
     .where({ id })
@@ -135,6 +110,5 @@ module.exports = {
   add,
   remove,
   update,
-  updateSettings,
-  findByForUsername
+  updateSettings
 };
